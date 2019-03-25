170e9673b5dc6c28a128c6ef8b1a1a7c92efaf2d
Ankit <shukla147.1111@gmail.com>
2019-03-25 12:46:38 +0530

    PostResponse

--- app/src/main/java/com/concept/test/rest/response/PostResponse.java
+++ app/src/main/java/com/concept/test/rest/response/PostResponse.java
@@ -4,5 +4,35 @@ import com.concept.test.model.BaseDomain;
 
 public class PostResponse extends BaseDomain {
 
+    String postDetail,postTitle;
 
+    public PostResponse() {
+    }
+
+    public String getPostDetail() {
+        return postDetail;
+    }
+
+    public void setPostDetail(String postDetail) {
+        this.postDetail = postDetail;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponse{" +
+                "posttitle='" + postTitle + '\'' +
+                ", postdetail='" + postDetail + '\'' +
+                '}';
+    }
 }
+
+
+
